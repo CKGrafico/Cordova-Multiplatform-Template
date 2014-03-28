2aca49d26d0867b1312efa32f74473592d67cdac
Quique Fdez Guerra <quique@ckgrafico.com>
2014-03-28 01:21:22 +0100

    fixed create bugs

--- create/index.js
+++ create/index.js
@@ -1,7 +1,17 @@
 var ncp = require('./node_modules/ncp').ncp;
 
+var testGitName = function (name) {
+	if(name.search(/(\.git|\.bow)/g) > -1) {
+		return false;
+	}
+	return true;
+};
+
 var moveFiles = function(source, dir) {
-	ncp(source, dir, function (err) {
+	var options = {
+		filter: testGitName
+	};
+	ncp(source, dir, options, function (err) {
 		if (err) {
 			return console.error(err);
 		}
