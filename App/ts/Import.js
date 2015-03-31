48b57192a5b4a898c57b3e4a0c1dd974b05bb7cf
CKGrafico <quique@ckgrafico.com>
2015-03-31 16:38:36 +0200

    Remove js files

--- App/ts/Import.js
+++
@@ -1,5 +0,0 @@
-/// <reference path="lib/typings/definitelytyped/angularjs/angular.d.ts" />
-/// <reference path="lib/typings/definitelytyped/cordova/cordova.d.ts" />
-/// <reference path="lib/typings/definitelytyped/cordova-ionic/cordova-ionic.d.ts" />
-/// <reference path="lib/typings/definitelytyped/angular-ui-router/angular-ui-router.d.ts" />
-//# sourceMappingURL=Import.js.map
