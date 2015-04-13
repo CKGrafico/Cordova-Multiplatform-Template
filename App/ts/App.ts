e16ebf4fd57762e8531ef6737ca51f52b6792712
ckgrafico <CBR600rr>
2015-04-13 15:40:09 +0200

    reset for security

---
+++ App/ts/App.ts
@@ -0,0 +1,111 @@
+﻿/// <reference path="imports.ts" />
+
+module App {
+    'use strict';
+
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    function onDeviceReady() {
+        angular.module('App', ['ionic'])
+            .controller('navigationController', App.NavigationController)
+            .controller('actionsController', App.ActionsController)
+            .config(['$stateProvider', '$urlRouterProvider', statesConfiguration])
+            .config(['$httpProvider', httpInterceptor])
+            .run(['$rootScope', '$ionicLoading', httpInterceptorActions])
+            .config(['$compileProvider', function ($compileProvider) {
+			$compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|file|mailto|ms-appx):/);
+		}]);
+
+        angular.bootstrap(document.querySelector('body'), ['App']);
+    }
+
+    // Configure routes
+    function statesConfiguration($stateProvider: ng.ui.IStateProvider, $urlRouterProvider: ng.ui.IUrlRouterProvider): void {
+
+        $stateProvider
+        // Tabs Menu
+        .state('tabs', {
+            url: "/tab",
+            abstract: true,
+            templateUrl: "templates/partials/tabs.html"
+        })
+
+        // Side Menu Left
+            .state('tabs.left', {
+            url: "/left",
+            views: {
+                'left-tab': {
+                    templateUrl: "templates/pages/left.html"
+                }
+            }
+        })
+        
+        // Home Views
+       .state('tabs.home', {
+            url: "/home",
+            views: {
+                'home-tab': {
+                    templateUrl: "templates/pages/home.html"
+                }
+            }
+        })
+        .state('tabs.scroll', {
+            url: "/scroll",
+            views: {
+                'home-tab': {
+                    templateUrl: "templates/pages/scroll.html"
+                }
+            }
+        })
+
+        // actions Views
+        .state('tabs.actions', {
+            url: "/actions",
+            views: {
+                'actions-tab': {
+                    controller: 'actionsController',
+                    templateUrl: "templates/pages/actions.html"
+                }
+            }
+        })
+
+        // buttons Views
+        .state('tabs.buttons', {
+            url: "/buttons",
+            views: {
+                'buttons-tab': {
+                    templateUrl: "templates/pages/buttons.html"
+                }
+            }
+        })
+
+        $urlRouterProvider.otherwise("/tab/home");
+    }
+
+    // Configure interceptor
+    function httpInterceptor($httpProvider: ng.IHttpProvider) {
+        $httpProvider.interceptors.push(function ($rootScope) {
+            return {
+                request: function (config) {
+                    $rootScope.$broadcast('loading:show')
+                    return config
+                },
+                response: function (response) {
+                    $rootScope.$broadcast('loading:hide')
+                    return response
+                }
+            }
+        })
+    }
+
+    // Configure interceptor actions
+    function httpInterceptorActions($rootScope: ng.IRootScopeService, $ionicLoading: Ionic.ILoading) {
+        $rootScope.$on('loading:show', function () {
+            $ionicLoading.show({ templateUrl: "templates/partials/loading.html"})
+        })
+
+        $rootScope.$on('loading:hide', function () {
+            $ionicLoading.hide()
+        })
+    }
+}
---
+++ App/ts/controllers/actionsController.ts
@@ -0,0 +1,33 @@
+﻿/// <reference path="../imports.ts" />
+
+module App {
+    'use strict';
+
+    export class ActionsController {
+
+
+        public static $inject = [
+            '$scope',
+            '$http'
+        ];
+
+        constructor(
+            private $scope: any,
+            private $http: any
+            ) {
+
+            $scope.exampleAction = () => this.exampleAction();
+            $scope.property = 'Void';
+
+            $http.jsonp('http://api.openbeerdatabase.com/v1/breweries.json?callback=JSON_CALLBACK').then(function (result) {
+                var a = result.data.breweries
+            })
+
+        }
+
+        private exampleAction() {
+            this.$scope.property = 'Clicked';
+        }
+    }
+} 
+
