4769026d88977003e6667a339cf9d86edf9ff501
ckgrafico <quique@ckgrafico.com>
2015-08-09 11:34:46 +0200

    Add support for controller as

--- App/ts/controllers/actionsController.ts
+++ App/ts/controllers/actionsController.ts
@@ -5,29 +5,28 @@ module App {
 
     export class ActionsController {
 
-
         public static $inject = [
             '$scope',
             '$http'
         ];
+
+        private property: string = 'Void';
 
         constructor(
             private $scope: IActionsScope,
             private $http: ng.IHttpService
             ) {
 
-            $scope.exampleAction = () => this.exampleAction();
-            $scope.property = 'Void';
-
             $http.jsonp('http://api.openbeerdatabase.com/v1/breweries.json?callback=JSON_CALLBACK').then(function (result) {
                 console.log(result);
             })
-
         }
 
         private exampleAction() {
-            this.$scope.property = 'Clicked';
+            this.property = 'Clicked';
         }
     }
+
+    angular.module('App').controller('actionsController', ActionsController);
 } 
 
