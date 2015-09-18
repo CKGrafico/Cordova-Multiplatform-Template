fddadab7a3f8fe1aca16478f22234740a81c8cd2
Quique Fernandez <e.fernandez@plainconcepts.com>
2015-09-18 13:47:33 +0200

    Update gulpfile
    // Working without Visual Studio
        // 1.- npm install
        // 2.- gulp initialize
        // 3.- gulp watch
    
    // Working with Visual Studio
        // 1.- npm install
        // 2.- gulp initialize

--- App/gulpfile.js
+++ App/gulpfile.js
@@ -1,8 +1,9 @@
-﻿/// <binding BeforeBuild='default' />
+﻿/// <binding BeforeBuild='default-vs' />
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 var runSequence = require('run-sequence');
 var mainBowerFiles = require('main-bower-files');
+var tsProject = plugins.typescript.createProject('scripts/tsconfig.json');
 
 var files = {
     css: 'css/**/*.css',
@@ -37,156 +38,175 @@ function getCorrectPaths(folder, filesa, filesb) {
     return cfiles;
 }
 
-// Tasks
-gulp.task('default', function () {
-    runSequence('default:scss', 'default:inject');
-});
+// Working without Visual Studio
+    // 1.- npm install
+    // 2.- gulp initialize
+    // 3.- gulp watch
 
-gulp.task('initialize', function () {
-    runSequence('initialize:bower');
-    // 1 Initialize
-    // 2 Build Visual Studio Project
-    // 3 Default
-});
+// Working with Visual Studio
+    // 1.- npm install
+    // 2.- gulp initialize
 
-gulp.task('build', function () {
-    runSequence('build:js', 'build:clean');
-});
-
-gulp.task('zip', function () {
-    runSequence('zip:copy', 'zip:compress', 'zip:clean');
-});
-
-// Filter node packages
-gulp.task('initialize:bower', ['initialize:bower:install'], function () {
-    gulp.src(mainBowerFiles('**/*.scss'), { base: 'bower_components' })
-		.pipe(gulp.dest(paths.project + paths.scss + '/lib'));
-		
-    gulp.src(mainBowerFiles('**/*.js'), { base: 'bower_components' })
-		.pipe(gulp.dest(paths.project + paths.www + '/' + paths.js + '/lib'));
+// Tasks definition
+    gulp.task('default', function () {
+        // Compile Sass, TypeScript and inject files into index.html
+        runSequence('default:scss', 'default:ts', 'default:inject');
+    });
     
-    gulp.src(mainBowerFiles('**/*.d.ts'), { base: 'bower_components' })
-		.pipe(gulp.dest(paths.project + paths.ts + '/lib/typings'));
+    gulp.task('default-vs', function () {
+        // Compile Sass, inject files into index.html (for Visual Studio)
+        runSequence('default:scss', 'default:inject');
+    });
     
-    return gulp.src(mainBowerFiles('**/fonts/**.*'), { base: 'bower_components' })
-		.pipe(gulp.dest(paths.project + paths.www + '/fonts/lib'));
+    gulp.task('initialize', function () {
+        // Download and install bower packages
+        runSequence('initialize:bower');
+    });
     
-    //return gulp.src(mainBowerFiles(['**/*.css', '**/*.scss']), { base: 'bower_components' })
-		//.pipe(plugins.minifyCss({ keepSpecialComments: 0 }))// Because sass import fails with variables in comments
-		//.pipe(plugins.rename(function (path) {
-    //    path.extname = ".scss"
-    //}))
-		//.pipe(gulp.dest(paths.project + '/' + paths.scss + '/lib'));
-});
+    gulp.task('build', function () {
+        // Build project
+        runSequence('build:js', 'build:clean');
+    });
+    
+    gulp.task('zip', function () {
+        // Zip template (only for dev purposes)
+        runSequence('zip:copy', 'zip:compress', 'zip:clean');
+    });
 
-// Install bower dependencies
-gulp.task('initialize:bower:install', function () {
-    return plugins.bower();
-});
-
-// Inject JS & CSS Files
-gulp.task('default:inject', function () {
-    return gulp.src(paths.project + paths.www + '/' + files.index)
-		.pipe(plugins.inject(
-        gulp.src(getCorrectPaths(paths.project + paths.www + '/', files.css, files.js), { read: false }),
-			{
-            transform: function (filepath) {
-                if (filepath.indexOf('.js') > -1) {
+// Tasks implementation
+    // Download bower dependencies
+    gulp.task('initialize:bower:install', function () {
+        return plugins.bower();
+    });
+    
+    // Move bower packages to each folder
+    gulp.task('initialize:bower', ['initialize:bower:install'], function () {
+        gulp.src(mainBowerFiles('**/*.scss'), { base: 'bower_components' })
+            .pipe(gulp.dest(paths.project + paths.scss + '/lib'));
+            
+        gulp.src(mainBowerFiles('**/*.js'), { base: 'bower_components' })
+            .pipe(gulp.dest(paths.project + paths.www + '/' + paths.js + '/lib'));
+        
+        gulp.src(mainBowerFiles('**/*.d.ts'), { base: 'bower_components' })
+            .pipe(gulp.dest(paths.project + paths.ts + '/lib/typings'));
+        
+        return gulp.src(mainBowerFiles('**/fonts/**.*'), { base: 'bower_components' })
+            .pipe(gulp.dest(paths.project + paths.www + '/fonts/lib'));
+        
+        //return gulp.src(mainBowerFiles(['**/*.css', '**/*.scss']), { base: 'bower_components' })
+            //.pipe(plugins.minifyCss({ keepSpecialComments: 0 }))// Because sass import fails with variables in comments
+            //.pipe(plugins.rename(function (path) {
+        //    path.extname = ".scss"
+        //}))
+            //.pipe(gulp.dest(paths.project + '/' + paths.scss + '/lib'));
+    });
+    
+    // Inject files into index
+    gulp.task('default:inject', function () {
+        return gulp.src(paths.project + paths.www + '/' + files.index)
+            .pipe(plugins.inject(
+            gulp.src(getCorrectPaths(paths.project + paths.www + '/', files.css, files.js), { read: false }),
+                {
+                transform: function (filepath) {
+                    if (filepath.indexOf('.js') > -1) {
+                        return '<script src="' + filepath.replace(paths.project + paths.www + '/', '').substring(1) + '"></script>'
+                    }
+                    // Css
+                    return ' <link rel="stylesheet" href="' + filepath.replace(paths.project + paths.www + '/', '').substring(1) + '">'
+                }
+            }
+        ))
+            .pipe(gulp.dest(paths.project + paths.www));
+    });
+    
+    // Compile Sass
+    gulp.task('default:scss', function () {
+        return gulp.src(paths.project + files.scss)
+            .pipe(plugins.sass())
+            .pipe(plugins.autoprefixer({
+            browsers: ['last 2 version', 'android 4']
+        }))
+            .pipe(gulp.dest(paths.project + paths.www + '/' + paths.css));
+    });
+    
+    // Compile Typescript
+    gulp.task('default:ts', function () {
+        return tsProject.src()
+            .pipe(plugins.typescript(tsProject))
+            .pipe(gulp.dest(paths.project));
+    });
+    
+    // Build JavaScript
+    gulp.task('build:js', function () {
+        return gulp.src(paths.project + paths.www + '/' + files.index)
+            .pipe(plugins.inject(
+            gulp.src(getCorrectPaths(paths.project + paths.www + '/', files.js))
+                    .pipe(plugins.concat('scripts/build.min.js'))
+                    .pipe(plugins.uglify())
+                    .pipe(gulp.dest(paths.project + paths.www)),
+                {
+                transform: function (filepath) {
                     return '<script src="' + filepath.replace(paths.project + paths.www + '/', '').substring(1) + '"></script>'
                 }
-                // Css
-                return ' <link rel="stylesheet" href="' + filepath.replace(paths.project + paths.www + '/', '').substring(1) + '">'
             }
-        }
-    ))
-		.pipe(gulp.dest(paths.project + paths.www));
-});
-
-// Compile Sass
-gulp.task('default:scss', function () {
-    return gulp.src(paths.project + files.scss)
-		.pipe(plugins.sass())
-		.pipe(plugins.autoprefixer({
-        browsers: ['last 2 version', 'android 4']
-    }))
-		.pipe(gulp.dest(paths.project + paths.www + '/' + paths.css));
-});
-
-// Build JavaScript
-gulp.task('build:js', function () {
-    return gulp.src(paths.project + paths.www + '/' + files.index)
-		.pipe(plugins.inject(
-        gulp.src(getCorrectPaths(paths.project + paths.www + '/', files.js))
-                .pipe(plugins.concat('scripts/build.min.js'))
-                .pipe(plugins.uglify())
-                .pipe(gulp.dest(paths.project + paths.www)),
-			{
-            transform: function (filepath) {
-                return '<script src="' + filepath.replace(paths.project + paths.www + '/', '').substring(1) + '"></script>'
-            }
-        }
-    ))
-     .pipe(gulp.dest(paths.project + paths.www))
-});
-
-// Build Clean
-gulp.task('build:clean', function () {
-    return gulp.src(getCorrectPaths(paths.project + paths.www + '/', files.js, files.maps), { read: false })
-        .pipe(plugins.clean({ force: true }));
-});
-
-// Create a Zip template for VS
-gulp.task('zip:copy', function () {
-    var dest = '../zipCreatorTemp';
-    gulp.src(['../__TemplateIcon.ico', '../LICENSE', '../*.md', '../*.sln', '../*.vstemplate'])
-        .pipe(plugins.clone())
-        .pipe(gulp.dest(dest));
-        
-    //gulp.src('../Tasks/*.*')
-    //    .pipe(plugins.clone())
-    //    .pipe(gulp.dest(dest + '/Tasks'));
-        
-    gulp.src('*.*')
-        .pipe(plugins.clone())
-        .pipe(gulp.dest(dest + '/App'));
-        
-    gulp.src('merges/**/*.*')
-        .pipe(plugins.clone())
-        .pipe(gulp.dest(dest + '/App/merges'));
-        
-    gulp.src('plugins/**/*.*')
-        .pipe(plugins.clone())
-        .pipe(gulp.dest(dest + '/App/plugins'));
+        ))
+        .pipe(gulp.dest(paths.project + paths.www))
+    });
     
-    gulp.src('res/**/*.*')
-        .pipe(plugins.clone())
-        .pipe(gulp.dest(dest + '/App/res'));
+    // Build Clean
+    gulp.task('build:clean', function () {
+        return gulp.src(getCorrectPaths(paths.project + paths.www + '/', files.js, files.maps), { read: false })
+            .pipe(plugins.clean({ force: true }));
+    });
+    
+    // Create a Zip template for VS
+    gulp.task('zip:copy', function () {
+        var dest = '../zipCreatorTemp';
+        gulp.src(['../__TemplateIcon.ico', '../LICENSE', '../*.md', '../*.sln', '../*.vstemplate'])
+            .pipe(plugins.clone())
+            .pipe(gulp.dest(dest));
+            
+        gulp.src('*.*')
+            .pipe(plugins.clone())
+            .pipe(gulp.dest(dest + '/App'));
+            
+        gulp.src('merges/**/*.*')
+            .pipe(plugins.clone())
+            .pipe(gulp.dest(dest + '/App/merges'));
+            
+        gulp.src('plugins/**/*.*')
+            .pipe(plugins.clone())
+            .pipe(gulp.dest(dest + '/App/plugins'));
         
-    gulp.src(['scss/**/*.*', '!scss/lib/**/*.*'])
-        .pipe(plugins.clone())
-        .pipe(gulp.dest(dest + '/App/scss'));
-        
-    gulp.src(['scripts/**/*.*', '!scripts/lib/**/*.*'])
-        .pipe(plugins.clone())
-        .pipe(gulp.dest(dest + '/App/scripts'));
-        
-    return gulp.src(['www/**/*.*', '!www/fonts/lib/**/*.*', '!www/css/**/*.*', '!www/scripts/appbundle.*'])
-        .pipe(plugins.clone())
-        .pipe(gulp.dest(dest + '/App/www'));
-});
-
-var zip;
-gulp.task('zip:compress', function () {
-    var dest = '../zipCreatorTemp';
-    var d = new Date(Date.now());
-    zip = 'Cordova-' + d.getDate() + '_' + (d.getMonth()+1) + '_' + d.getFullYear() + '-' + d.getHours() + '_' + d.getMinutes() + '_' + d.getSeconds();
-    return gulp.src(dest + '/**/*')
-        .pipe(plugins.zip(zip + '.zip'))
-        .pipe(gulp.dest('../'));
-});
-
-gulp.task('zip:clean', function () {   
-    var dest = '../zipCreatorTemp';
-    return gulp.src(dest, {read: false})
-        .pipe(plugins.clean({force: true}))
-});
+        gulp.src('res/**/*.*')
+            .pipe(plugins.clone())
+            .pipe(gulp.dest(dest + '/App/res'));
+            
+        gulp.src(['scss/**/*.*', '!scss/lib/**/*.*'])
+            .pipe(plugins.clone())
+            .pipe(gulp.dest(dest + '/App/scss'));
+            
+        gulp.src(['scripts/**/*.*', '!scripts/lib/**/*.*'])
+            .pipe(plugins.clone())
+            .pipe(gulp.dest(dest + '/App/scripts'));
+            
+        return gulp.src(['www/**/*.*', '!www/fonts/lib/**/*.*', '!www/css/**/*.*', '!www/scripts/appbundle.*'])
+            .pipe(plugins.clone())
+            .pipe(gulp.dest(dest + '/App/www'));
+    });
+    
+    var zip;
+    gulp.task('zip:compress', function () {
+        var dest = '../zipCreatorTemp';
+        var d = new Date(Date.now());
+        zip = 'Cordova-' + d.getDate() + '_' + (d.getMonth()+1) + '_' + d.getFullYear() + '-' + d.getHours() + '_' + d.getMinutes() + '_' + d.getSeconds();
+        return gulp.src(dest + '/**/*')
+            .pipe(plugins.zip(zip + '.zip'))
+            .pipe(gulp.dest('../'));
+    });
+    
+    gulp.task('zip:clean', function () {   
+        var dest = '../zipCreatorTemp';
+        return gulp.src(dest, {read: false})
+            .pipe(plugins.clean({force: true}))
+    });
