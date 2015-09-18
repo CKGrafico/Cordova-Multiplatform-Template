72c31e0d431f93714b52c7274ab31a7ee23436c3
ckgrafico <quique@ckgrafico.com>
2015-09-18 16:24:00 +0200

    Fix import.ts links

--- App/scripts/imports.ts
+++ App/scripts/imports.ts
@@ -1,9 +1,9 @@
 ﻿// External dependencies
 
 // d.ts
-/// <reference path="lib/typings/definitelytyped/angularjs/angular.d.ts" />
-/// <reference path="lib/typings/definitelytyped/angular-ui-router/angular-ui-router.d.ts" />
-/// <reference path="lib/typings/ionic-typescript-definitions/beta14/ionic.d.ts" />
-/// <reference path="lib/typings/definitelytyped/cordova/cordova.d.ts" />
-/// <reference path="lib/typings/definitelytyped/cordova-ionic/cordova-ionic.d.ts" />
+/// <reference path="lib/typings/angularjs/angular.d.ts" />
+/// <reference path="lib/typings/angular-ui-router/angular-ui-router.d.ts" />
+/// <reference path="lib/ionic/ionic.d.ts" />
+/// <reference path="lib/typings/cordova/cordova.d.ts" />
+/// <reference path="lib/typings/cordova-ionic/cordova-ionic.d.ts" />
 
