406f0a55539a87801307f4cd75dfe850161cbb7d
Quique Fernandez <e.fernandez@plainconcepts.com>
2016-07-22 17:08:10 +0200

    First version simple of webpack

---
+++ App/webpack.config.js
@@ -0,0 +1,34 @@
+﻿// TO DO improve
+var webpack = require('webpack');
+var HtmlWebPackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/main.ts',
+        vendor: ['./node_modules/ionic-angular/release/js/ionic.bundle.js'],
+        scss: require('./src/main.scss')
+    },
+    output: {
+        path: './www',
+        filename: '[name].bundle.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.ts$/, loader: 'ts' },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract(['css', 'sass'])
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.ts', '.scss']
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html'
+        }),
+        new ExtractTextPlugin('[name].css')
+    ]
+};
