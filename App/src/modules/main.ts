a9e0353df3e0db2a15d6766fa7c08fcf46e03dcb
Quique Fernandez <e.fernandez@plainconcepts.com>
2016-09-08 19:25:27 +0200

    Comment config

--- App/src/modules/main.ts
+++ App/src/modules/main.ts
@@ -1,1 +1,5 @@
 ﻿const a = 1;
+angular.module('app', []).
+    run(function (Servicetest) {
+        console.log('test');
+    });
--- App/webpack.config.js
+++ App/webpack.config.js
@@ -17,6 +17,7 @@ module.exports = {
     module: {
         preLoaders: [
             {
+                // TypeScript Linting
                 test: /\.ts$/,
                 loader: 'tslint'
             }
@@ -24,23 +25,29 @@ module.exports = {
 
         loaders: [
             {
+                // Transpile TypeScript
+                // Add angular annotations
                 test: /\.ts$/,
-                loader: 'ts'
+                loader: ['ts']
             },
             {
+                // Transpile SCSS
                 test: /\.scss$/,
                 loader: ExtractTextPlugin.extract(['css', 'sass'])
             },
             {
+                // Move images
                 test: /\.(jpe?g|png|gif|svg)$/i,
                 loaders: ['file?name=' + config.files.output.images, 'img']
             }
         ]
     },
     plugins: [
+        // Inject into HTML
         new HtmlWebPackPlugin({
             template: config.files.index
         }),
+        // Create css bundle
         new ExtractTextPlugin(config.files.output.css)
     ]
 };
